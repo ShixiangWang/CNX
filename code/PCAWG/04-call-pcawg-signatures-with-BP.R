fff62d77ddfa2a58e87bb88a77857e7d73ca1f6d
ShixiangWang <wangshx@shanghaitech.edu.cn>
2020-12-07 17:34:55 +0800

    添加 signature profile 和优化的 activity

--- code/PCAWG/04-call-pcawg-signatures-with-BP.R
+++ code/PCAWG/04-call-pcawg-signatures-with-BP.R
@@ -3,26 +3,24 @@ library(sigminer)
 tally_X <- readRDS("data/pcawg_cn_tally_X.rds")
 tally_X_noLOH <- readRDS("data/pcawg_cn_tally_X_noLOH.rds")
 
-dim(tally_X$nmf_matrix)
-
-library(profvis)
-debug(bp_extract_signatures)
-
-sigs <- bp_extract_signatures(
-  tally_X$nmf_matrix,
-  range = 2:30,
-  n_bootstrap = 5,
-  n_nmf_run = 5,
-  cores = 16,
-  cores_solution = 4,
-  one_batch = TRUE,
-  cache_dir = "BP/BP_PCAWG_test",
-  keep_cache = TRUE,
-  only_core_stats = TRUE
-)
-
-bp_show_survey(sigs, fixed_ratio = F, add_score = FALSE)
-saveRDS(sigs, file = "data/pcawg_cn_sigs_CN176_BP_test.rds")
+# library(profvis)
+# debug(bp_extract_signatures)
+#
+# sigs <- bp_extract_signatures(
+#   tally_X$nmf_matrix,
+#   range = 2:30,
+#   n_bootstrap = 5,
+#   n_nmf_run = 5,
+#   cores = 16,
+#   cores_solution = 4,
+#   one_batch = TRUE,
+#   cache_dir = "BP/BP_PCAWG_test",
+#   keep_cache = TRUE,
+#   only_core_stats = TRUE
+# )
+#
+# bp_show_survey(sigs, fixed_ratio = F, add_score = FALSE)
+# saveRDS(sigs, file = "data/pcawg_cn_sigs_CN176_BP_test.rds")
 
 library(NMF)
 sigs <- sig_extract(tally_X$nmf_matrix, 10, nrun = 100, cores = 16)
@@ -53,3 +51,49 @@ shapiro.test(expo_rel$Sig1)
 #   cores = 16
 # )
 # saveRDS(sigs2, file = "data/pcawg_cn_sigs_CN136_BP_test.rds")
+
+sigs <- sig_extract(tally_X$nmf_matrix, 11, nrun = 100, cores = 10)
+saveRDS(sigs, file = "data/pcawg_cn_sigs_CN176_11sigs.rds")
+
+# Compare BP result and Standard NMF --------------------------------------
+
+library(sigminer)
+solution1000 <- readRDS("BP/BP_PCAWG_1000_Extraction_Result.rds")
+bp_show_survey(solution1000, add_score = F, fixed_ratio = F)
+
+solution100 <- readRDS("data/pcawg_cn_sigs_CN176_BP.rds")
+bp_show_survey(solution100, add_score = F, fixed_ratio = F)
+
+sigs <- readRDS("data/pcawg_cn_sigs_CN176_11sigs.rds")
+
+s1 <- get_sig_similarity(solution1000$object$K11, sigs)
+s2 <- get_sig_similarity(solution100$object$K11, sigs)
+s3 <- get_sig_similarity(solution1000$object$K11, solution100$object$K11)
+
+ComplexHeatmap::pheatmap(s1$similarity, cluster_cols = F, cluster_rows = F)
+ComplexHeatmap::pheatmap(s2$similarity, cluster_cols = F, cluster_rows = F)
+ComplexHeatmap::pheatmap(s3$similarity, cluster_cols = F, cluster_rows = F)
+
+mean(subset(solution1000$stats_sample, signature_number == 11)$cosine_distance_mean)
+mean(subset(solution100$stats_sample, signature_number == 11)$cosine_distance_mean)
+# Optimizing exposures ----------------------------------------------------
+
+library(sigminer)
+tally_X <- readRDS("data/pcawg_cn_tally_X.rds")
+pcawg_types <- readRDS("data/pcawg_type_info.rds")
+
+sc <- pcawg_types$cancer_type
+names(sc) <- pcawg_types$sample
+
+pcawg_expo <- bp_attribute_activity(
+  solution1000$object$K11,
+  sample_class = sc,
+  nmf_matrix = tally_X$nmf_matrix,
+  return_class = "data.table"
+)
+
+saveRDS(pcawg_expo, file = "data/pcawg_cn_sigs_CN176_activity.rds")
+
+pcawg_expo$abs_activity
+summary(pcawg_expo$similarity)
+
