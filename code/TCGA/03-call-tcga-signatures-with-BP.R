e1feb2b73b85bb58c07370c6262c7fbc18374afc
ShixiangWang <wangshx@shanghaitech.edu.cn>
2020-12-17 09:58:47 +0800

    updata landscape analysis

--- code/TCGA/03-call-tcga-signatures-with-BP.R
+++ code/TCGA/03-call-tcga-signatures-with-BP.R
@@ -1,5 +1,7 @@
 library(sigminer)
 
+setwd("~/projects/CNX/")
+
 tally_X <- readRDS("data/TCGA/tcga_cn_tally_X.rds")
 tally_X_noLOH <- readRDS("data/TCGA/tcga_cn_tally_X_noLOH.rds")
 
@@ -18,12 +20,13 @@ tcga_expo <- bp_attribute_activity(
   tcga_solutions$object$K11,
   sample_class = sc,
   nmf_matrix = tally_X$nmf_matrix,
-  return_class = "data.table"
+  return_class = "data.table",
+  use_parallel = 12
 )
 
 saveRDS(tcga_expo, file = "data/TCGA/tcga_cn_sigs_CN176_activity.rds")
 
-hist(tcga_expo$similarity, breaks = 100)
+# hist(tcga_expo$similarity, breaks = 100)
 
 # mat <- get_sig_similarity(pcawg_sigs, tcga_solutions$object$K11)
 # pheatmap::pheatmap(mat$similarity, cluster_cols = F, cluster_rows = F)
