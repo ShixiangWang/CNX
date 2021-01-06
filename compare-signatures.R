4551012b41eba50280e294fd55e8f9248ae9d6b3
ShixiangWang <wangshx@shanghaitech.edu.cn>
2021-01-06 13:57:47 +0800

    record some plays

--- compare-signatures.R
+++ compare-signatures.R
@@ -29,3 +29,70 @@ pheatmap::pheatmap(sim_cross$similarity, cluster_rows = F, cluster_cols = F, dis
 
 sim_cross2 <- get_sig_similarity(SP_PCAWG2$solution_list$S17, SA_PCAWG)
 pheatmap::pheatmap(sim_cross2$similarity, cluster_rows = F, cluster_cols = F, display_numbers = TRUE)
+
+
+# SP and BP ---------------------------------------------------------------
+
+SP_TCGA2 <- sigprofiler_import("SP/TCGA_CN176X/", order_by_expo = TRUE, type = "all")
+BP_PCAWG <- readRDS("data/pcawg_cn_sigs_CN176_signature.rds")
+
+sig10 <- readRDS("data/pcawg_cn_sigs_CN176_10sigs.rds")
+sig11 <- readRDS("data/pcawg_cn_sigs_CN176_11sigs.rds")
+
+get_sig_similarity(sig11, sig10)
+get_sig_similarity(sig11, BP_PCAWG)
+
+sim_list <- lapply(SP_TCGA2$solution_list[-1], function(x) {
+  diag(get_sig_similarity(BP_PCAWG, x)$similarity[1:11, 1:11])
+})
+
+sort(sapply(sim_list, mean), decreasing = TRUE)
+
+sim <- get_sig_similarity(BP_PCAWG, SP_TCGA2$solution_list$S14)
+pheatmap(sim$similarity, display_numbers = TRUE, cluster_rows = FALSE)
+
+
+sim <- get_sig_similarity(SP_PCAWG2$solution_list$S10, SP_TCGA2$solution_list$S11)
+pheatmap(sim$similarity, display_numbers = TRUE, cluster_rows = FALSE)
+
+show_sig_profile(SP_PCAWG2$solution_list$S10, mode = "copynumber", method = "X", style = "cosmic")
+show_sig_profile(SP_TCGA2$solution_list$S11, mode = "copynumber", method = "X", style = "cosmic")
+
+sim <- get_sig_similarity(SP_PCAWG2$solution_list$S10, sig10)
+pheatmap(sim$similarity, display_numbers = TRUE, cluster_rows = FALSE)
+
+sim_list <- lapply(SP_TCGA2$solution_list, function(x) {
+  diag(get_sig_similarity(SP_PCAWG2$solution_list$S10, x)$similarity[1:10, 1:10])
+})
+
+sort(sapply(sim_list, mean), decreasing = TRUE)
+
+
+# sim_list2 <- lapply(SP_TCGA2$solution_list, function(x) {
+#   get_sig_similarity(SP_PCAWG2$solution_list$S10, x)$similarity[1:10, 1:10]
+# })
+#
+# sim_list2$S10
+
+plot(10:30, sapply(sim_list, function(x) x[8]))
+#
+sim <- get_sig_similarity(SP_PCAWG2$solution_list$S10, BP_PCAWG)
+pheatmap(sim$similarity, display_numbers = TRUE, cluster_rows = FALSE, cluster_cols = FALSE)
+
+sim <- get_sig_similarity(SP_PCAWG2$solution_list$S14, BP_PCAWG)
+pheatmap(sim$similarity, display_numbers = TRUE, cluster_rows = FALSE, cluster_cols = FALSE)
+
+sim <- get_sig_similarity(SP_PCAWG2$solution_list$S10, SP_TCGA2$solution_list$S11)
+pheatmap(sim$similarity, display_numbers = TRUE, cluster_rows = FALSE, cluster_cols = FALSE)
+
+mean(diag(sim$similarity))
+
+sim <- get_sig_similarity(SP_PCAWG2$solution_list$S10, SP_TCGA2$solution_list$S18)
+pheatmap(sim$similarity, display_numbers = TRUE, cluster_rows = FALSE, cluster_cols = FALSE)
+
+mean(diag(sim$similarity))
+
+sim <- get_sig_similarity(SP_PCAWG2$solution_list$S14, SP_TCGA2$solution_list$S20)
+pheatmap(sim$similarity, display_numbers = TRUE, cluster_rows = FALSE, cluster_cols = FALSE)
+mean(diag(sim$similarity))
+
