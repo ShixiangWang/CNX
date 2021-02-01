c3549db44f5e77941e0d60d787bda9cef13283fa
ShixiangWang <wangshx@shanghaitech.edu.cn>
2021-02-01 14:47:30 +0800

    print TCGA CNS

--- CNS-profile.R
+++ CNS-profile.R
@@ -36,6 +36,19 @@ p <- show_sig_profile_loop(pcawg_sig,
 ggplot2::ggsave("output/pcawg_cn_sigs_loop_c176_by_context.pdf", plot = p, width = 14, height = 30)
 
 
+tcga_sigs <- readRDS("data/TCGA/tcga_cn_sigs_CN176_signature.rds")
+sapply(get_sig_exposure(tcga_sigs)[, -1], sum)
+
+p <- show_sig_profile_loop(tcga_sigs,
+                           x_lab = NULL,
+                           style = "cosmic",
+                           mode = "copynumber",
+                           method = "X",
+                           font_scale = 0.6, by_context = TRUE)
+
+ggplot2::ggsave("output/tcga_cn_sigs_loop_c176_by_context.pdf", plot = p, width = 14, height = 40)
+
+
 # One representative sample -----------------------------------------------
 
 s <- "SP112845"
@@ -63,3 +76,6 @@ p <- show_sig_profile(pcawg_sig, sig_names = paste0("CNS", c(3, 8, 10, 11, 14)),
                       style = "cosmic", mode = "copynumber", method = "X",
                       by_context = TRUE,  font_scale = 0.7)
 ggplot2::ggsave("output/showcase_sig_profile.pdf", plot = p, width = 14, height = 5)
+
+x = get_sig_exposure(pcawg_sig, type = "relative")
+round(x[sample == "SP112845"][,-1], 3)
