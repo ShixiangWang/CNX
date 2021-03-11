618d067a1931e4d7efbfd83adcefcf65b15db392
ShixiangWang <wangshx@shanghaitech.edu.cn>
2021-03-11 15:07:54 +0800

    Add methon comparison analysis and CNA burden pancan dist

--- CNA-burden-group_analysis.R
+++ CNA-burden-group_analysis.R
@@ -1,17 +1,31 @@
+# 先按 cancer_type 画分布即可
+
+library(ggridges)
+
+tidy_info <- readRDS("data/pcawg_sample_tidy_info.rds")
+
+p <- ggplot(tidy_info, aes(x = cna_burden, y = cancer_type, fill = cancer_type)) +
+  geom_density_ridges() +
+  labs(x = "CNA burden", y = NULL) +
+  theme_ridges() +
+  theme(legend.position = "none")
+
+ggsave("output/cna_burden_pancan_dist.pdf", plot = p, width = 6, height = 7)
+
 # Split samples into groups based on CNA burden distribution
 
-library(flexmix)
-
-N1 <- 100
-N2 <- 10
-
-a <- rpois(N1, 0)
-b <- rpois(N2, 50)
-
-x <- c(a,b)
-class <- c(rep('a', N1), rep('b', N2))
-data <- data.frame(cbind(x=as.numeric(x), class=as.factor(class)))
-
-fit3 <- flexmix(x ~ 1, data = data, k = 2, model = flexmix::FLXMCmvpois())
-fit3 <- flexmix(x ~ 1, data = data, k = 2, model = FLXMCmvnorm())
-clusters(fit3)
+# library(flexmix)
+#
+# N1 <- 100
+# N2 <- 10
+#
+# a <- rpois(N1, 0)
+# b <- rpois(N2, 50)
+#
+# x <- c(a,b)
+# class <- c(rep('a', N1), rep('b', N2))
+# data <- data.frame(cbind(x=as.numeric(x), class=as.factor(class)))
+#
+# fit3 <- flexmix(x ~ 1, data = data, k = 2, model = flexmix::FLXMCmvpois())
+# fit3 <- flexmix(x ~ 1, data = data, k = 2, model = FLXMCmvnorm())
+# clusters(fit3)
--- comparison-between-method-W-and-X.R
+++ comparison-between-method-W-and-X.R
@@ -1,8 +1,7 @@
 # Use PCAWG Breast and OV data as example datasets to
 # compare method W and X in
-# 1) reconstructed profile RSS and cosine similarity
-# 2) the difference between estimated segments and observed segments
-# 3) the mean similarity within copy number signatures
+# 1) the difference between estimated segments and observed segments
+# 2) the mean similarity within copy number signatures
 #
 # Here we set the signature number from 3 to 10.
 
@@ -41,3 +40,99 @@ for (type in types) {
 }
 
 save(ResultX, ResultW, file = "Comparison_Sig_Data.RData")
+
+
+# Comparison --------------------------------------------------------------
+
+# within similarity
+get_within_similarity <- function(x) {
+  sim <- suppressMessages(get_sig_similarity(x, x, set_order = FALSE))$similarity
+  sim[upper.tri(sim)]
+}
+
+CrossSimX <- map_df(ResultX, function(x) {
+  map(x, get_within_similarity) %>% set_names(paste0(3:10, "_Sigs")) %>%
+    enframe("sig_type", "sim") %>%
+    unnest("sim")
+}, .id = "cancer_type")
+
+
+CrossSimW <- map_df(ResultW, function(x) {
+  map(x, get_within_similarity) %>% set_names(paste0(3:10, "_Sigs")) %>%
+    enframe("sig_type", "sim") %>%
+    unnest("sim")
+}, .id = "cancer_type")
+
+CrossSim <- bind_rows(
+  CrossSimX %>% mutate(method = "X"),
+  CrossSimW %>% mutate(method = "W")
+)
+
+library(ggpubr)
+p <- ggboxplot(CrossSim %>%
+                 mutate(
+                   sig_type = stringr::str_replace(sig_type, "_Sigs", "")
+                 ),
+               x = "sig_type", y = "sim", fill = "method", facet.by = "cancer_type",
+               xlab = "Extracted signature number", ylab = "Similarity between each other") +
+  stat_compare_means(aes(group = method, label = ..p.signif..))
+ggsave("output/similarity_comparison_between_methods.pdf", plot = p, width = 12, height = 7)
+
+cn_obj <- readRDS("data/pcawg_cn_obj.rds")
+
+observed_count <- cn_obj@summary.per.sample[, .(sample, n_of_seg)]
+
+get_estimated_count <- function(x) {
+  get_sig_exposure(x) %>%
+    mutate(count = rowSums(.[, -1])) %>%
+    select(sample, count)
+}
+
+count_df <- map2_df(ResultX, ResultW, function(x, y) {
+  countX <- map(x, get_estimated_count) %>% set_names(paste0(3:10, "_Sigs")) %>%
+    data.table::rbindlist(idcol = "sig_type")
+  countX$method = "X"
+  countW <- map(y, get_estimated_count) %>% set_names(paste0(3:10, "_Sigs")) %>%
+    data.table::rbindlist(idcol = "sig_type")
+  countW$method = "W"
+  rbind(countX, countW)
+}, .id = "cancer_type")
+
+count_df2 <- left_join(count_df, observed_count, by = "sample")
+
+save(CrossSim, count_df2, file = "Comparison_Result_Data.RData")
+
+ggscatter(count_df2 %>% filter(method == "W"), x = "n_of_seg", y = "count", facet.by = "cancer_type",
+          xlab = FALSE, ylab = FALSE) +
+  geom_abline(slope = 1, color = "red")
+ggscatter(count_df2 %>% filter(method == "X"), x = "n_of_seg", y = "count", facet.by = "cancer_type",
+          xlab = FALSE, ylab = FALSE) +
+  geom_abline(slope = 1, color = "red")
+
+count_summary <- count_df2 %>%
+  group_by(cancer_type, sig_type, sample, method) %>%
+  summarise(rmse = sqrt(sum((count - n_of_seg)^2) / length(count)),
+            change_frac = 100 * (rmse / n_of_seg),
+            .groups = "drop")
+
+count_summary
+
+p <- ggboxplot(count_summary %>%
+            mutate(
+              sig_type = as.integer(stringr::str_replace(sig_type, "_Sigs", ""))
+            ),
+          x = "sig_type", y = "rmse", fill = "method", size = 0.5, width = 0.5, outlier.size = 0.5,
+          facet.by = "cancer_type", scales = "free_y",
+          xlab = "Extracted signature number", ylab = "RMSE between observed segments and estimated segments") +
+  stat_compare_means(aes(group = method, label = ..p.signif..))
+ggsave("output/estimation_RMSE_comparison_between_methods.pdf", plot = p, width = 12, height = 7)
+
+p <- ggboxplot(count_summary %>%
+                 mutate(
+                   sig_type = as.integer(stringr::str_replace(sig_type, "_Sigs", ""))
+                 ),
+               x = "sig_type", y = "change_frac", fill = "method", size = 0.5, width = 0.5, outlier.size = 0.5,
+               facet.by = "cancer_type", scales = "free_y",
+               xlab = "Extracted signature number", ylab = "Change between observed segments and estimated segments (%)") +
+  stat_compare_means(aes(group = method, label = ..p.signif..))
+ggsave("output/estimation_change_frac_comparison_between_methods.pdf", plot = p, width = 12, height = 7)
